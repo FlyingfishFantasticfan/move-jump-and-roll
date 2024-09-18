6c9dca8163c51feafd83b27afff90c0e952305c2
FlyingfishFantasticfan <yiweifan92@gmail.com>
2024-09-18 13:18:38 +0800

    add hider scene

--- project/scripts/hide.gd
+++ project/scripts/hide.gd
@@ -1,7 +1,17 @@
 extends TileMapLayer
 
+@onready var area_2d: Area2D = $Area2D
+
+var defualt_color :Color
+var entered_color :Color
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	defualt_color = modulate
+	entered_color = modulate
+	entered_color.a = 0.3
+	area_2d.body_entered.connect(_on_area_2d_body_entered)
+	area_2d.body_exited.connect(_on_area_2d_body_exited)
 	pass # Replace with function body.
 
 
@@ -11,10 +21,11 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	visible = false
+	print("玩家进入")
+	modulate = entered_color
 	pass # Replace with function body.
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	visible = true
+	modulate = defualt_color
 	pass # Replace with function body.
