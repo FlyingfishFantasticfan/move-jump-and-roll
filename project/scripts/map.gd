16c57c07d1d63a47f948a28bca41c56eae0a7cb4
FlyingfishFantasticfan <yiweifan92@gmail.com>
2024-09-22 22:08:04 +0800

    添加了结算

--- project/scripts/map.gd
+++ project/scripts/map.gd
@@ -3,8 +3,9 @@ extends Node2D
 @onready var camera: Camera2D = $Player/Camera
 @onready var player: CharacterBody2D = $Player
 @onready var coins: Node2D = $Mid/Coins
+@onready var label: Label = $Player/Camera2D/Label
 
-var heath : int = 5
+var num_dead : int = 0
 var score : int = 0
 var num_coins: int = 0
 
@@ -13,6 +14,7 @@ var is_winning : bool =false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
+	label.visible = false
 	for child in coins.get_children():
 		if child.name.begins_with("coin"):
 			num_coins += 1
@@ -24,6 +26,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit"):
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
 	if is_winning:
+		label.visible = true
 		player.set_physics_process(false)
 		if Input.is_action_just_pressed("resume"):
 			get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -39,6 +42,12 @@ func _on_coin_take_up() -> void:
 
 func _on_win_zone_win() -> void:
 	is_winning = true
+	label.text = "You Win\n\n" +"Dead: "+str(num_dead)+"\n\n"+ "Coins: " + str(score) +" / "+str(num_coins)+"\n\n"+"Press Space to continue"
 	if score == num_coins:
 		print("full")
+	pass # Replace with function body.
+
+
+func _on_player_dead() -> void:
+	num_dead += 1
 	pass # Replace with function body.
--- project/scripts/win_zone.gd
+++ project/scripts/win_zone.gd
@@ -1,6 +1,11 @@
 extends Area2D
 signal win
 
+
+
+
+	
 func _on_body_entered(body: Node2D) -> void:
-	win.emit()
+	emit_signal("win")
+
 	pass # Replace with function body.
