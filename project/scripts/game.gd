a4e1807c8e88c1ce92cbb0f5bbb72498e0cbd62a
FlyingfishFantasticfan <yiweifan92@gmail.com>
2024-10-01 16:09:22 +0800

    解决了信号连接问题，把所有地图加入选关

--- project/scripts/game.gd
+++ project/scripts/game.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 
-const MAP_2 = preload("res://scenes/levels/map_2.tscn")
-const MAP_1 = preload("res://scenes/levels/map_1.tscn")
+const MAP_3 = preload("res://scenes/levels/map_1.tscn")
+const MAP_1 = preload("res://scenes/levels/map_2.tscn")
+const Map_2 = preload("res://scenes/levels/map_3.tscn")
+const Map_4 = preload("res://scenes/levels/map_4.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,20 +13,24 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("关卡1"):
-		get_tree().change_scene_to_file(MAP_1.resource_path)
-	if Input.is_action_just_pressed("关卡2"):
-		get_tree().change_scene_to_file(MAP_2.resource_path)
-	
+
 
 
 func _on_button_2_button_down() -> void:
-	get_tree().change_scene_to_file(MAP_1.resource_path)
+	get_tree().change_scene_to_file(Map_2.resource_path)
 	pass # Replace with function body.
 
 
 func _on_button_button_down() -> void:
-	get_tree().change_scene_to_file(MAP_2.resource_path)
+	get_tree().change_scene_to_file(MAP_1.resource_path)
+	pass # Replace with function body.
+
+
+func _on_button_3_button_down() -> void:
+	get_tree().change_scene_to_file(MAP_3.resource_path)
+	pass # Replace with function body.
+
+
+func _on_button_4_button_down() -> void:
+	get_tree().change_scene_to_file(Map_4.resource_path)
 	pass # Replace with function body.
