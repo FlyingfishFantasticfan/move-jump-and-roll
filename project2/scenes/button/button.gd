45fa007d8a760d60228781681e406b9d46f342b0
FlyingfishFantasticfan <yiweifan92@gmail.com>
2024-12-30 23:28:49 +0800

    修复了按钮bug

--- project2/scenes/button/button.gd
+++ project2/scenes/button/button.gd
@@ -8,6 +8,8 @@ var is_on : bool = false:
 	set(x):
 		is_on = x
 		sprite_2d.texture = BUTTON_ON if is_on else  BUTTON_OFF
+		if connected != null:
+			connected.is_on = is_on
 
 @onready var timer: Timer = $Timer
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -18,17 +20,14 @@ func _ready() -> void:
 	timer.wait_time = wait_time
 	sprite_2d.texture = BUTTON_ON if is_on else  BUTTON_OFF
 
-func _physics_process(delta: float) -> void:
-	if connected != null:
-		connected.is_on = is_on
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		is_on = true
+		timer.stop()
 
 func _on_timer_timeout() -> void:
 	is_on = false
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player&&is_timing:
+	if body is Player&&is_timing&&is_on:
 		timer.start()
