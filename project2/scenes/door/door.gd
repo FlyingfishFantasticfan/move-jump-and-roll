c967fdbb89843eaf101f2f71a3c78d841e86e000
FlyingfishFantasticfan <yiweifan92@gmail.com>
2025-01-01 11:58:50 +0800

    修复了按钮bug

--- project2/scenes/door/door.gd
+++ project2/scenes/door/door.gd
@@ -3,8 +3,9 @@ const DOOR_OFF = preload("res://assets/door_off.png")
 const DOOR_ON = preload("res://assets/door_on.png")
 
 var is_on = false
-@onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $Sprite2D/StaticBody2D/CollisionShape2D
 
 func _physics_process(delta: float) -> void:
 	collision_shape_2d.disabled = is_on
